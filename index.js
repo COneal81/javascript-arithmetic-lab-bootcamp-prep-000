e086de3487e121f2efa18ad86904a649440d18be
triadfirewood@gmail.com <triadfirewood@gmail.com>
2018-06-19 19:04:32 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+var numberA = "7"
+var numberB = "22"
+
+function add (numberA, numberB){
+  return(numberA+=numberB)
+}
+
+function subtract(numberB, numberA) {
+  return(numberB-=numberA)
+}
+
+function multiply(numberA, numberB) {
+  return(numberA*=numberB)
+}
+
+function divide(numberB, numberA) {
+  return(numberB/=numberA)
+}
+var n = "797"
+function inc(n){
+  return (++n)
+}
+
+function dec(n) {
+  return(--n)
+}
+
+function makeInt(n) {
+  return(parseInt(n))
+}
+
